c78e0e21d926248eff16dbd24918e03cabae0cba
Faisal Nisar <fnisar394@protonmail.com>
2022-11-17 09:42:10 +0500

    changed name from trivia to fact

--- pkg/factlist/service.go
+++ pkg/factlist/service.go
@@ -7,7 +7,7 @@ import (
 	"github.com/markhaur/trivia/pkg"
 )
 
-// Service is an application service that lets us interact with a list of trivias
+// Service is an application service that lets us interact with a list of facts
 type Service interface {
 	Save(context.Context, pkg.Fact) (*pkg.Fact, error)
 	List(context.Context) ([]pkg.Fact, error)
@@ -26,34 +26,34 @@ func NewService(repository pkg.FactRepository) Service {
 	return &service{repository: repository}
 }
 
-func (s *service) Save(ctx context.Context, trivia pkg.Fact) (*pkg.Fact, error) {
-	if err := s.repository.Insert(ctx, &trivia); err != nil {
-		return nil, fmt.Errorf("could not save trivia: %v", err)
+func (s *service) Save(ctx context.Context, fact pkg.Fact) (*pkg.Fact, error) {
+	if err := s.repository.Insert(ctx, &fact); err != nil {
+		return nil, fmt.Errorf("could not save fact: %v", err)
 	}
-	return &trivia, nil
+	return &fact, nil
 }
 
 func (s *service) List(ctx context.Context) ([]pkg.Fact, error) {
 	list, err := s.repository.FindAll(ctx)
 	if err != nil {
-		return nil, fmt.Errorf("could not list all trivias: %v", err)
+		return nil, fmt.Errorf("could not list all facts: %v", err)
 	}
 	return list, nil
 }
 
-func (s *service) Update(ctx context.Context, trivia pkg.Fact) (*pkg.Fact, bool, error) {
-	err := s.repository.Update(ctx, &trivia)
+func (s *service) Update(ctx context.Context, fact pkg.Fact) (*pkg.Fact, bool, error) {
+	err := s.repository.Update(ctx, &fact)
 	if err == pkg.ErrFactNotFound {
-		err = s.repository.Insert(ctx, &trivia)
+		err = s.repository.Insert(ctx, &fact)
 		if err != nil {
-			return nil, false, fmt.Errorf("could not create trivia: %v", err)
+			return nil, false, fmt.Errorf("could not create fact: %v", err)
 		}
-		return &trivia, true, nil
+		return &fact, true, nil
 	}
 	if err != nil {
-		return nil, false, fmt.Errorf("could not update task: %v", err)
+		return nil, false, fmt.Errorf("could not update fact: %v", err)
 	}
-	return &trivia, false, nil
+	return &fact, false, nil
 }
 
 func (s *service) Remove(ctx context.Context, id int64) error {
@@ -61,7 +61,7 @@ func (s *service) Remove(ctx context.Context, id int64) error {
 		if err == pkg.ErrFactNotFound {
 			return err
 		}
-		return fmt.Errorf("could not remove trivia: %v", err)
+		return fmt.Errorf("could not remove fact: %v", err)
 	}
 	return nil
 }
